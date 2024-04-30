b89bda2aea3a3871b67f281b4f84327ebd89b96d
En Loyd <eew16589@gmail.com>
2024-04-30 15:03:54 +0800

    Updated db

--- include/config.php
+++ include/config.php
@@ -2,7 +2,7 @@
 defined('server') ? null : define("server", "localhost");
 defined('user') ? null : define ("user", "root") ;
 defined('pass') ? null : define("pass","");
-defined('database_name') ? null : define("database_name", "cemeterymappingdb") ;
+defined('database_name') ? null : define("database_name", "darbchps_db") ;
 
 $this_file = str_replace('\\', '/', __File__) ;
 $doc_root = $_SERVER['DOCUMENT_ROOT'];
